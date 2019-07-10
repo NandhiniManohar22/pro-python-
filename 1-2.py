449a3c06ae569077aacd708095413f320a362ce1
NandhiniManohar22 <51949323+NandhiniManohar22@users.noreply.github.com>
2019-07-10 16:45:52 +0530

    Update 1-2.py

--- 1-2.py
+++ 1-2.py
@@ -1,8 +1,8 @@
 from itertools import combinations
 n1,n2=input().split()
 n2=int(n2)
-char=[]
-nan=combinations(n1,len(n1)-n2)
-for n in nan:
-  char.append("".join(n))
-  print(min(char))
+sha=[]
+hue=combinations(n1,len(n1)-n2)
+for n in hue:
+  sha.append("".join(n))
+  print(min(sha))
