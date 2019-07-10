3646f191ae83b9f409cbea029af7b457f93c2cdb
NandhiniManohar22 <51949323+NandhiniManohar22@users.noreply.github.com>
2019-07-10 16:24:48 +0530

    Update 1-1.py

--- 1-1.py
+++ 1-1.py
@@ -1,8 +1,8 @@
 def longest(n1,n2):
-  if n1 in n2:
-      return n1
+  if(n1 in n2):
+     return n1
   else:
-     return longest(k[0:len[k]-1],n2)
+     return longest(n1[0:len[n1]-1],n2)
 num=int(input())
 char=[]
 for _ in range(0,num):
